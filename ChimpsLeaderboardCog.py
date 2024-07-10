b1a4aa81d2f6212379a9e18734f8120f306155fb
joantiscar <joantiscar@gmail.com>
2024-07-10 19:47:17 +0200

    Added basic commands: SeeScores and AddScore

--- ChimpsLeaderboardCog.py
+++ ChimpsLeaderboardCog.py
@@ -1,27 +1,59 @@
 from discord.ext import commands
+import json
 import discord
 import logging
 import os
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='bot.log', filemode='a')
-class AddScore(commands.Cog, name="Add score"):
+class ChimpsScoring(commands.Cog, name="Chimps scoring"):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='add_score')
-    async def add_score(self, ctx):
+    async def add_score(self, ctx, player, map, round):
         """
-        Usage: !add_score map round_number. Also, send a screenshot in the same message 
+        Usage: !add_score playerName map round 
         """
         try:
             with open('chimps_scores.json', 'r', encoding="utf-8") as file:
                 scores = json.load(file)
-                scores_ids = sorted([score["id"] for score in scores])
+                text = ""
+                scoresList = scores.get("scores", "putamierda")
+                data =  {}
+                data["user"]= player
+                data["map"]= map
+                data["round"]= round
+                data["message"] = "test"
+                scoresList.append(data) 
+                scores["scores"] = scoresList
+                with open('chimps_scores.json', 'w', encoding='utf-8') as file:
+                    json.dump(scores, file, ensure_ascii=False, indent=4)  # Use ensure_ascii=False to write UTF-8 characters directly
+                await ctx.send("Score saved")
         except FileNotFoundError:
-            await ctx.send("No Гроздан. Blame Pollo.")
+            await ctx.send("No scores file found. Blame Bola")
+    @commands.command(name='see_scores')
+    async def see_scores(self, ctx):
+        """
+        Usage: !see_scores 
+        """
+        try:
+            with open('chimps_scores.json', 'r', encoding="utf-8") as file:
+                scores = json.load(file)
+                text = ""
+                scoresList = scores.get("scores", "putamierda")
+                for score in scoresList:
+                    text = text + "User: " + score["user"]
+                    text = text + "Map: " + score["map"]
+                    text = text + "Round: " + score["round"]
+                    text = text + "Message: " + score["message"]
+                await ctx.send(text)
+        except FileNotFoundError:
+            await ctx.send("No scores file found. Blame Bola")
+
+    
 
     # ------------------------------------------------------------------------------------------------------------------------------------------
 
 
 async def setup(bot):
-    await bot.add_cog(AddScore(bot))
+    await bot.add_cog(ChimpsScoring(bot))
